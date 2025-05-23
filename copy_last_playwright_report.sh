f293f7203321509eb829b5982ed71d51df04cfd8
Lukasz Bola <lukasz.bola@icloud.com>
2025-05-23 22:00:06 +0200

    Refactor copy_last_playwright_report.sh to use variables for directory paths and improve readability

--- copy_last_playwright_report.sh
+++ copy_last_playwright_report.sh
@@ -1,11 +1,16 @@
 # This script is used to download a zip file from a given URL, unzip it, and move the unzipped contents to a specified directory with a timestamp.
 timestamp=$(date +%Y%m%d%H%M%S)
-new_zip=~/Downloads/playwright-report-$timestamp.zip
-mv ~/Downloads/playwright-report.zip $new_zip
-if [ -d /tmp/playwright-report-unzipped ]; then
-    rm -rf /tmp/playwright-report-unzipped/*
+DOWNLOADS_DIR=~/Downloads
+UNZIP_DIR=/tmp/playwright-report-unzipped
+DEST_DIR=~/Documents/jaktestowac/projects/demo-bank-test
+ZIP_FILE=$DOWNLOADS_DIR/playwright-report-$timestamp.zip
+REPORT_DIR=$DEST_DIR/playwright-report-$timestamp
+
+mv $DOWNLOADS_DIR/playwright-report.zip $ZIP_FILE
+if [ -d $UNZIP_DIR ]; then
+    rm -rf $UNZIP_DIR/*
 fi
-unzip -q $new_zip -d /tmp/playwright-report-unzipped
-mkdir -p /Users/lukaszbola/Documents/jaktestowac/projects/demo-bank-test/playwright-report-$timestamp/
-cp -R /tmp/playwright-report-unzipped/* /Users/lukaszbola/Documents/jaktestowac/projects/demo-bank-test/playwright-report-$timestamp/
-rm -rf ~/Downloads/playwright-report*
+unzip -q $ZIP_FILE -d $UNZIP_DIR
+mkdir -p $REPORT_DIR/
+cp -R $UNZIP_DIR/* $REPORT_DIR/
+rm -rf $DOWNLOADS_DIR/playwright-report*
